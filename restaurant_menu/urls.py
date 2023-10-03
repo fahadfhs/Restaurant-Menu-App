108ff74230de4a791ead5d3a86a118a9443f1e0c
fahadfhs <90725219+fahadfhs@users.noreply.github.com>
2023-10-03 11:35:39 -0400

    used context understanding to send python data to the html page using dictionary

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -4,4 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.MenuList.as_view(), name='home')  # the '' is for the home page, as_view() renders the class as view
 
-]
+ ]
--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -9,6 +9,10 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by("-date_created")  # orders the object by the query
     template_name = "index.html"  # returns the template as requested
 
+    def get_context_data(self):
+        context = {"meals": "Pizza"}
+        return context
+
 
 # this class displays detail of each item
 class MenuItemDetail(generic.DetailView):
