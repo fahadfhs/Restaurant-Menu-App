9cb87b56189599dbe926dfc819573cfa705b275c
fahadfhs <90725219+fahadfhs@users.noreply.github.com>
2023-10-03 11:20:36 -0400

    created two class based views, each of them renders a particular template. Set up url pattern for the url of the app under the urls.py under the main app as home. Also registered the app.urls path under my site under the url file.

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', include('restaurant_menu.urls')) # registering app url link here, '' means top url sub directory
 ]
--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+# creating class based views
+from django.views import generic
+from .models import Item
 
-# Create your views here.
+
+# this class displays the list of menu items
+class MenuList(generic.ListView):
+    queryset = Item.objects.order_by("-date_created")  # orders the object by the query
+    template_name = "index.html"  # returns the template as requested
+
+
+# this class displays detail of each item
+class MenuItemDetail(generic.DetailView):
+    model = Item
+    template_name = "menu_item_detail.html"
