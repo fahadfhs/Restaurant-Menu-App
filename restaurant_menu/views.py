57ad6e0054fd58d49194bfd435f5b6832dfc3e80
fahadfhs <90725219+fahadfhs@users.noreply.github.com>
2023-10-04 13:27:17 -0400

    edited the code for jinja in html to use for loop to iterate over meals and show meal.1(first item) and then further show description of each meal in the front end. Changed the context function to make it dynamic and created a dictionary with kwargs super()

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 # creating class based views
 from django.views import generic
-from .models import Item
+from .models import Item, MEAL_TYPE
 
 
 # this class displays the list of menu items
@@ -9,8 +9,9 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by("-date_created")  # orders the object by the query
     template_name = "index.html"  # returns the template as requested
 
-    def get_context_data(self):
-        context = {"meals": "Pizza"}
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs) # instead of hardcode we start with empty dic
+        context["meals"] = MEAL_TYPE
         return context
 
 
