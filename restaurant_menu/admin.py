8edc0fdc7f4850f175801e501e17a2ac244e9e2d
fahadfhs <90725219+fahadfhs@users.noreply.github.com>
2023-10-04 12:22:40 -0400

    created admin interface and the different way in which the data would be displayed in it. Registered the admin interface. Created new admin in terminal and added items in it through the front end django

--- restaurant_menu/admin.py
+++ restaurant_menu/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+
+class MenuItemAdmin(admin.ModelAdmin):  # below showcases in what specification items will be accessed
+    list_display = ("meal", "status")
+    list_filter = ("status",)
+    search_fields = ("meal", "description")
+
+
+admin.site.register(Item, MenuItemAdmin)
