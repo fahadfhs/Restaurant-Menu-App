d57864bc22b8e2566b338a39266cbdf32b157f63
fahadfhs <90725219+fahadfhs@users.noreply.github.com>
2023-10-05 01:48:24 -0400

    displayed item details in the href when clicked in the main page. The items were made links in the front page. Also added another path in urls.py as a subdirectory which uses int:pk to get the items on the main screen. T

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MenuList.as_view(), name='home')  # the '' is for the home page, as_view() renders the class as view
-
- ]
+    path('', views.MenuList.as_view(), name='home'),  # the '' is for the home page, as_view() renders the class as view
+    path('item/<int:pk>/', views.MenuItemDetail.as_view(), name="menu_item")  # another view for handling individual
+    # subdirectory
+]
